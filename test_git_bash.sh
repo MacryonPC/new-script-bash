dd8d5701407bd0d955de42c7714c3adad1f32b36
macryon <philmacro41@gmail.com>
2026-07-05 16:18:34 +0300

    add new file script bash

---
+++ test_git_bash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+green='\033[0;32m'
+red='\033[0;31m'
+yellow='\033[0;33m'
+nc='\033[0m' #
+
+
+main (){
+	echo -e "${red}hello world "
+	echo -e "I have a new water"
+
+
+}
+#запуск приложения 
+main
+
